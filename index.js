a45bbe2d0642e78013e1d52fa771971c1340a95d
Mr. steal your set#8959 <125122145+scyllastrikes@users.noreply.github.com>
2023-07-23 12:20:31 +0100

    Start interface + card distribution(schema)

--- index.js
+++ index.js
@@ -2,30 +2,49 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let dealerEl = document.getElementById("dealer-el")
+let playerEl = document.getElementById("player-el")
 let randomNumber = Math.floor(Math.random() * 11) + 1
 let startBtn= document.getElementById("start-btn")
 let hitBtn= document.getElementById("hit-btn")
 let standBtn= document.getElementById("stand-btn")
+var serveSound = document.getElementById("servingAudio");
+
 hitBtn.style.display = "none";
 standBtn.style.display = "none";
+let dealerCards=[]
+let playerCards=[]
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+async function loading(arg){  
+    messageEl.textContent=arg
+    for (let i = 0; i < 3; i++){
+        await delay(300)
+        messageEl.textContent=messageEl.textContent+"."
+     }
+    serveSound.play();
+}
 function startGame() {
     startBtn.style.display = "none";
+    dealerCards.push(Math.floor(Math.random() * 11) + 1)
+    playerCards.push(Math.floor(Math.random() * 11) + 1)
+    loading("dealing the facedown card")
+    dealerEl.textContent="Dealers hand: "+dealerCards[0]
+    loading("dealing to the player")
+    loading("dealing the faceup dealer card ")
+
+        
+    
+    playerEl.textContent="Dealers hand: "+playerCards[0]
+
+
+
+
+    
+    
     hitBtn.style.display = "block";
-standBtn.style.display = "block";
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
+    standBtn.style.display = "block";
 }
 
 // 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
